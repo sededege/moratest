32a729ce903e2845f62d0b5c7f162c9db4e99c4e
Sebastian Gonzalez <74278554+sededege@users.noreply.github.com>
2024-08-23 20:59:57 -0300

    Update Ordenes.js

--- src/components/producto/Ordenes.js
+++ src/components/producto/Ordenes.js
@@ -25,7 +25,7 @@ const Ordenes = () => {
     if (a === "malvin") {
       return <p>Av. Italia 4240. Apto 1910</p>;
     } else if (a === "trescruces") {
-      return <p>Duvimioso Terra 2234</p>;
+      return <p>Cassinoni 1684, entre Colonia y Victor Haedo. Tres Cruces.</p>;
     } else {
       return <p>Envio</p>;
     }
